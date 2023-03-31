d6efbd2d1737db3b2007984568321b1c6c8ef673
Nicolás Olivera <nnicolivera@hotmail.com>
2023-03-31 16:41:31 -0300

    notes backend to deploy

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+];
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find(note => note.id === id);
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    }
+});
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        /*
+    notes.map(n => n.id) creates a new array that contains all the ids of the notes. Math.max returns the maximum value of the numbers that are passed to it. However, notes.map(n => n.id) is an array so it can't directly be given as a parameter to Math.max. The array can be transformed into individual numbers by using the "three dot" spread syntax ....
+        */
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1;
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body;
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        });
+    }
+    const note = {
+        id: generateId(),
+        content: body.content,
+        important: body.important || false
+    }
+    notes = notes.concat(note);
+    response.json(note);
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    notes = notes.filter(note => note.id !== id);
+    response.status(204).end();
+});
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:  ', request.path);
+    console.log('Body:  ', request.body);
+    console.log('---');
+    next();
+}
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+}
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
